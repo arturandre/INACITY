f9831c23d293ebf397671fc8da2b3d3c42a26976
Artur <arturpossible@gmail.com>
2018-04-11 20:09:03 -0300

    first commit

--- django_website/django_website/GIS/MapMiner.py
+++ django_website/django_website/GIS/MapMiner.py
@@ -6,7 +6,7 @@ import Bound, StreetDTO, AmenityDTO
 from typing import List
 
 class MapMiner:
-    """Abstract class representing a Map Miner user to collect data from some GIS (Geographic Information System)."""
+    """Abstract class representing a Map Miner to collect data from some GIS (Geographic Information System)."""
     def __init__(self):
         raise NotImplementedError( "Abstract classes cannot be instantiated." )
     def getStreets(region: Bound) -> List[type(StreetDTO)]:
