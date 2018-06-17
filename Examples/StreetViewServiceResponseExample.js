b814546aaadbc3c853880926c90a40aaebc153d8
Andre <arturpossible@gmail.com>
2018-06-17 19:19:16 -0300

    More documentation with jsdoc

---
+++ Examples/StreetViewServiceResponseExample.js
@@ -0,0 +1,61 @@
+[//Feature Level
+    [//Coordinate Level
+        {
+            "location": {
+                "lat": -23.56025493803828,
+                "lon": -46.731298892134646,
+                "shortDescription": "1576 R. do Lago",
+                "description": "1576 R. do Lago, São Paulo",
+                "pano": "-a6qbIWS7Op13QSWHAYzYA"
+            },
+            "copyright": "© 2018 Google",
+            "links": [
+                {
+                    "description": "R. do Lago",
+                    "heading": 120.0461196899414,
+                    "pano": "Cxv7wfS-nBnQ0VS7jTG9cg"
+                },
+                {
+                    "description": "R. do Lago",
+                    "heading": 306.2562561035156,
+                    "pano": "8H3Epil0hBEq5Q4OwUGc4g"
+                }
+            ],
+            "tiles": {
+                "centerHeading": 303.10009765625,
+                "originHeading": 303.10009765625,
+                "originPitch": 1.203826904296875,
+                "tileSize": {
+                    "width": 512,
+                    "height": 512,
+                    "f": "px",
+                    "b": "px"
+                },
+                "worldSize": {
+                    "width": 13312,
+                    "height": 6656,
+                    "f": "px",
+                    "b": "px"
+                }
+            },
+            "time": [
+                {
+                    "ng": "DmoP6P-FfMfh1Vug090CTg",
+                    "Af": "2010-01-01T02:00:00.000Z"
+                },
+                {
+                    "ng": "fWejzq16yZ7RnBz3TWcHcA",
+                    "Af": "2013-08-01T03:00:00.000Z"
+                },
+                {
+                    "ng": "WaVU1pJU-s9jqjwDC39cQQ",
+                    "Af": "2016-01-01T02:00:00.000Z"
+                },
+                {
+                    "ng": "-a6qbIWS7Op13QSWHAYzYA",
+                    "Af": "2017-03-01T03:00:00.000Z"
+                }
+            ]
+        }
+    ]
+]
