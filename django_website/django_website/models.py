d93644c6adc97adaeb6d24c548faa13fa16e763d
Artur André <arturpossible@gmail.com>
2019-02-27 13:37:24 -0300

    Id from Session table is varchar 40 / docker compose.yml contains instructions to load GeoSampa data

--- django_website/django_website/models.py
+++ django_website/django_website/models.py
@@ -1,6 +1,7 @@
 from django_website.Primitives.GeoSampa import GeoSampa_BusStops
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 class GeoImage(models.Model):
     """
@@ -19,6 +20,7 @@ class FilterResult(models.Model):
     presence = models.BooleanField()
 
 class Session(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     sessionName = models.CharField(max_length=256, unique=False)
     uimodelJSON = models.TextField()
