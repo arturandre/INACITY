8d193194105883eddab5fde4b87b066893f18ce2
Artur Andre <arturpossible@gmail.com>
2019-12-06 00:49:19 -0300

    manage.py command scripts fixed

--- django_website/django_website/management/commands/autocollectpanoramas.py
+++ django_website/django_website/management/commands/autocollectpanoramas.py
@@ -1,3 +1,5 @@
+from django.core.management.base import BaseCommand
+
 from GSVPanoramaManager.db import DBManager
 #import subprocess
 from time import sleep
@@ -10,72 +12,84 @@ from datetime import datetime
 current_browser = None
 x = None
 
-async def start_headless_browser():
-    import pyppeteer
-    global current_browser
-    browser = await pyppeteer.launch(args=['--no-sandbox'], headless=True)
-    page = await browser.newPage()
-    await page.goto('http://localhost/gsvpanoramacollector/link_browser/default/')
-    try:
-        print("await page.waitForNavigation({'waitUntil': 'load', 'timeout': 3000})")
-        await page.waitForNavigation({'waitUntil': 'load', 'timeout': 3000})
-        #print('waiting for navigation')
-        #await page.waitForNavigation({'timeout': 5000})
-    except Exception as e:
-        print(e)
+class Command(BaseCommand):
+    help = """
+    Starts a headless browser, queries for unfufilled panoramas
+    at the neo4j database, collects them using the browser
+    and finally stores the collected panorama at the neo4j
+    database, fulfilling the unfulfilled panoramas.
+
+    Requires pyppeteer
+    """
+
+
+    async def start_headless_browser(self):
+        import pyppeteer
+        global current_browser
+        browser = await pyppeteer.launch(args=['--no-sandbox'], headless=True)
+        page = await browser.newPage()
+        await page.goto('http://localhost/gsvpanoramacollector/link_browser/default/')
+        try:
+            self.stdout.write("await page.waitForNavigation({'waitUntil': 'load', 'timeout': 3000})")
+            await page.waitForNavigation({'waitUntil': 'load', 'timeout': 3000})
+            #self.stdout.write('waiting for navigation')
+            #await page.waitForNavigation({'timeout': 5000})
+        except Exception as e:
+            self.stdout.write(e)
+            
         
-    
 
-    
-    current_browser = browser
-    
+        
+        current_browser = browser
+        
 
 
-async def stop_headless_browser():
-    global current_browser
-    if current_browser is not None:
-        await current_browser.close()
+    async def stop_headless_browser(self):
+        global current_browser
+        if current_browser is not None:
+            await current_browser.close()
 
-#asyncio.get_event_loop().run_until_complete(start_headless_browser())
-#x = DBManager()
+    def handle(self, *args, **options):
+        #asyncio.get_event_loop().run_until_complete(start_headless_browser())
+        #x = DBManager()
 
-#c = '/usr/bin/chromium --headless --disable-gpu --no-sandbox --remote-debugging-port=9222 http://localhost/gsvpanoramacollector/link_browser/default/'.split(' ')
-#c = '/usr/bin/google-chrome-stable --headless --disable-gpu --no-sandbox --remote-debugging-port=9222 http://localhost/gsvpanoramacollector/link_browser/default/'
-#process = subprocess.Popen(c, stdout=subprocess.PIPE)
-
-#print('process opened, waiting 5 seconds to start...')
-#sleep(5)
-print('Starting!')
-iterations = 1
-while True:
-    print(f'Collecting...')
-    try:
-        asyncio.get_event_loop().run_until_complete(start_headless_browser())
-        print('x = DBManager()')
-        x = DBManager()
-        print('x._update_panorama_references()')
-        print(datetime.now().time())
-        x._update_panorama_references()
-        print(f'Success: {iterations}')
-        iterations += 1
-        if iterations % 20 == 0:
-            print(f'20 successes, restarting neo4j to avoid the memory leak issue...')
-            os.system('neo4j restart')
-            sleep(5)
-        print('x.close()')
-        x.close()
-        print('asyncio.get_event_loop().run_until_complete(stop_headless_browser())')
-        asyncio.get_event_loop().run_until_complete(stop_headless_browser())
-    except:
-        print(f'Failed (iteration {iterations}) updating trying to reset chromium')
-        #process.kill()
-        asyncio.get_event_loop().run_until_complete(stop_headless_browser())
-        if x is not None:
-            x.close()
+        #c = '/usr/bin/chromium --headless --disable-gpu --no-sandbox --remote-debugging-port=9222 http://localhost/gsvpanoramacollector/link_browser/default/'.split(' ')
+        #c = '/usr/bin/google-chrome-stable --headless --disable-gpu --no-sandbox --remote-debugging-port=9222 http://localhost/gsvpanoramacollector/link_browser/default/'
         #process = subprocess.Popen(c, stdout=subprocess.PIPE)
-        asyncio.get_event_loop().run_until_complete(start_headless_browser())
+
+        #self.stdout.write('process opened, waiting 5 seconds to start...')
         #sleep(5)
-    finally:
-        #process.kill()
-        asyncio.get_event_loop().run_until_complete(stop_headless_browser())
+        self.stdout.write('Starting!')
+        iterations = 1
+        while True:
+            self.stdout.write(f'Collecting...')
+            try:
+                asyncio.get_event_loop().run_until_complete(self.start_headless_browser())
+                self.stdout.write('x = DBManager()')
+                x = DBManager()
+                self.stdout.write('x._update_panorama_references()')
+                self.stdout.write(datetime.now().time())
+                x._update_panorama_references()
+                self.stdout.write(f'Success: {iterations}')
+                iterations += 1
+                if iterations % 20 == 0:
+                    self.stdout.write(f'20 successes, restarting neo4j to avoid the memory leak issue...')
+                    os.system('neo4j restart')
+                    sleep(5)
+                self.stdout.write('x.close()')
+                x.close()
+                self.stdout.write('asyncio.get_event_loop().run_until_complete(stop_headless_browser())')
+                asyncio.get_event_loop().run_until_complete(self.stop_headless_browser())
+            except:
+                self.stdout.write(f'Failed (iteration {iterations}) updating trying to reset chromium')
+                #process.kill()
+                asyncio.get_event_loop().run_until_complete(self.stop_headless_browser())
+                if x is not None:
+                    x.close()
+                #process = subprocess.Popen(c, stdout=subprocess.PIPE)
+                asyncio.get_event_loop().run_until_complete(self.start_headless_browser())
+                #sleep(5)
+            finally:
+                #process.kill()
+                asyncio.get_event_loop().run_until_complete(self.stop_headless_browser())
 
--- django_website/django_website/management/commands/export_neo4j_to_csv.py
+++ django_website/django_website/management/commands/export_neo4j_to_csv.py
@@ -1,213 +1,231 @@
+from django.core.management.base import BaseCommand
+
+
 from neo4j import GraphDatabase
 from GSVPanoramaManager import settings
 
-def format_panorama_csv(panorama_row):
-    pid = str(panorama_row[0])
-    pdict = panorama_row[1]
-    centerHeading = str(pdict.get('centerHeading', ''))
-    copyright = pdict.get('copyright', '')
-    description = pdict.get('description', '')
-    imageDate = pdict.get('imageDate', '')
-    location = str(pdict.get('location', ''))
-    originHeading = str(pdict.get('originHeading', ''))
-    originPitch = str(pdict.get('originPitch', ''))
-    pano = pdict.get('pano', '')
-    shortDescription = pdict.get('shortDescription', '')
-    return (
-        pid + ';' +
-        centerHeading + ';' +
-        copyright + ';' +
-        description + ';' +
-        imageDate + ';' +
-        location + ';' +
-        originHeading + ';' +
-        originPitch + ';' +
-        pano + ';' +
-        shortDescription
-    )
+class Command(BaseCommand):
+    help = """Creates four csv files each of which with
+    data from the neo4j dataset. The files are:
 
-def format_link_csv(link_row):
-    a_id = str(link_row[0])
-    b_id = str(link_row[1])
-    pdict = link_row[2]
-    heading = str(pdict.get('heading', ''))
-    description = pdict.get('description', '')
-    return (
-        a_id + ';' +
-        b_id + ';' +
-        heading + ';' +
-        description
-    )
-
-def format_view_csv(view_row):
-    p_id = str(view_row[0])
-    v_id = str(view_row[1])
-    pdict = view_row[2]
-    heading = str(pdict.get('heading', ''))
-    pitch = str(pdict.get('pitch', ''))
-    return (
-        p_id + ';' +
-        v_id + ';' +
-        heading + ';' +
-        pitch
-    )
-
-def format_filter_result_csv(filter_result_row):
-    v_id = str(filter_result_row[0])
-    f_id = str(filter_result_row[1])
-    pdict = filter_result_row[2]
-    filterId = pdict.get('filterId', '')
-    density = str(pdict.get('density', ''))
-    return (
-        v_id + ';' +
-        f_id + ';' +
-        filterId + ';' +
-        density
-    )
+    export_panoramas.csv -
+    export_links.csv - 
+    export_views.csv - 
+    export_filter_results.csv - 
+    """
 
 
-print("Opening neo4j connection")
-db_settings = settings.NEO4J_DATABASES['default']
-uri = 'bolt://' + db_settings['HOST'] + ':' + db_settings['PORT']
-auth = (db_settings['USER'], db_settings['PASSWORD'])
-_driver = GraphDatabase.driver(uri, auth=auth)
-print("Starting session for panoramas")
-with _driver.session() as session:
-    minmax_ids = session.run((
-        "MATCH (p:Panorama) "
-        "RETURN MIN(ID(p)), MAX(ID(p))"
-    ))
-    minmax_ids = minmax_ids.values()[0]
-    print(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
-    stepSize = 100000
-    lb = minmax_ids[0]
-    hb = min(lb + stepSize, minmax_ids[1])
-    while lb < minmax_ids[1]:
-        print(f"Collecting panoramas from {lb} to {hb}")
-        panoramas = session.run((
-            "MATCH (p:Panorama) "
-            "WHERE "
-            f"ID(p) >= {lb} "
-            "AND "
-            f"ID(p) <= {hb} "
-            "RETURN ID(p), properties(p)"
-        ))
+    def format_panorama_csv(self, panorama_row):
+        pid = str(panorama_row[0])
+        pdict = panorama_row[1]
+        centerHeading = str(pdict.get('centerHeading', ''))
+        copyright = pdict.get('copyright', '')
+        description = pdict.get('description', '')
+        imageDate = pdict.get('imageDate', '')
+        location = str(pdict.get('location', ''))
+        location = location.replace('POINT(', '').replace(')', '').replace(' ', ';')
+        originHeading = str(pdict.get('originHeading', ''))
+        originPitch = str(pdict.get('originPitch', ''))
+        pano = pdict.get('pano', '')
+        shortDescription = pdict.get('shortDescription', '')
+        return (
+            pid + ';' +
+            centerHeading + ';' +
+            copyright + ';' +
+            description + ';' +
+            imageDate + ';' +
+            location + ';' +
+            originHeading + ';' +
+            originPitch + ';' +
+            pano + ';' +
+            shortDescription
+        )
 
-        panoramas = panoramas.values()
-        
-        print(f"Writing panoramas from {lb} to {hb} to CSV file")
-        with open('export_panoramas.csv', 'a+') as expfile:
-            for panorama in panoramas:
-                csvpano = format_panorama_csv(panorama)
-                expfile.write(csvpano+'\n')
+    def format_link_csv(self, link_row):
+        id_link = str(link_row[0])
+        a_id = str(link_row[1])
+        b_id = str(link_row[2])
+        pdict = link_row[3]
+        heading = str(pdict.get('heading', ''))
+        description = pdict.get('description', '')
+        return (
+            id_link + ';' +
+            a_id + ';' +
+            b_id + ';' +
+            heading + ';' +
+            description
+        )
+
+    def format_view_csv(self, view_row):
+        p_id = str(view_row[0])
+        v_id = str(view_row[1])
+        pdict = view_row[2]
+        heading = str(pdict.get('heading', ''))
+        pitch = str(pdict.get('pitch', ''))
+        return (
+            p_id + ';' +
+            v_id + ';' +
+            heading + ';' +
+            pitch
+        )
+
+    def format_filter_result_csv(self, filter_result_row):
+        v_id = str(filter_result_row[0])
+        f_id = str(filter_result_row[1])
+        pdict = filter_result_row[2]
+        filterId = pdict.get('filterId', '')
+        density = str(pdict.get('density', ''))
+        return (
+            v_id + ';' +
+            f_id + ';' +
+            filterId + ';' +
+            density
+        )
+
+
+    def handle(self, *args, **options):
+        self.stdout.write("Opening neo4j connection")
+        db_settings = settings.NEO4J_DATABASES['default']
+        uri = 'bolt://' + db_settings['HOST'] + ':' + db_settings['PORT']
+        auth = (db_settings['USER'], db_settings['PASSWORD'])
+        _driver = GraphDatabase.driver(uri, auth=auth)
+        self.stdout.write("Starting session for panoramas")
+        with _driver.session() as session:
+            minmax_ids = session.run((
+                "MATCH (p:Panorama) "
+                "RETURN MIN(ID(p)), MAX(ID(p))"
+            ))
+            minmax_ids = minmax_ids.values()[0]
+            self.stdout.write(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
+            stepSize = 100000
+            lb = minmax_ids[0]
+            hb = min(lb + stepSize, minmax_ids[1])
+            while lb < minmax_ids[1]:
+                self.stdout.write(f"Collecting panoramas from {lb} to {hb}")
+                panoramas = session.run((
+                    "MATCH (p:Panorama) "
+                    "WHERE "
+                    f"ID(p) >= {lb} "
+                    "AND "
+                    f"ID(p) <= {hb} "
+                    "RETURN ID(p), properties(p)"
+                ))
+
+                panoramas = panoramas.values()
+                
+                self.stdout.write(f"Writing panoramas from {lb} to {hb} to CSV file")
+                with open('export_panoramas.csv', 'a+') as expfile:
+                    for panorama in panoramas:
+                        csvpano = self.format_panorama_csv(panorama)
+                        expfile.write(csvpano+'\n')
+                        #break
+
+                lb += stepSize+1
+                hb = min(lb + stepSize, minmax_ids[1])
+
+        self.stdout.write('Starting session for links')
+        with _driver.session() as session:
+            minmax_ids = session.run((
+                "MATCH (a:Panorama)-[l:link]-(b:Panorama) "
+                "RETURN MIN(ID(l)),MAX(ID(l))"
+            ))
+            minmax_ids = minmax_ids.values()[0]
+            self.stdout.write(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
+            stepSize = 100000
+            lb = minmax_ids[0]
+            hb = min(lb + stepSize, minmax_ids[1])
+            while lb < minmax_ids[1]:
+                self.stdout.write(f"Collecting links from {lb} to {hb}")
+                links = session.run((
+                    "MATCH (a:Panorama)-[l:link]->(b:Panorama) "
+                    "WHERE "
+                    f"ID(l) >= {lb} "
+                    "AND "
+                    f"ID(l) <= {hb} "
+                    "RETURN DISTINCT ID(l), ID(a), ID(b), properties(l)"
+                ))
+
+                links = links.values()
+                
+                self.stdout.write(f"Writing links from {lb} to {hb} to CSV file")
+                with open('export_links.csv', 'a+') as expfile:
+                    for link in links:
+                        csvlink = self.format_link_csv(link)
+                        expfile.write(csvlink+'\n')
+                        #break
+
+                lb += stepSize+1
+                hb = min(lb + stepSize, minmax_ids[1])
                 #break
 
-        lb += stepSize+1
-        hb = min(lb + stepSize, minmax_ids[1])
+        self.stdout.write('Starting session for views')
+        with _driver.session() as session:
+            minmax_ids = session.run((
+                "MATCH (p:Panorama)-[]->(v:View) "
+                "RETURN MIN(ID(v)),MAX(ID(v))"
+            ))
+            minmax_ids = minmax_ids.values()[0]
+            self.stdout.write(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
+            stepSize = 100000
+            lb = minmax_ids[0]
+            hb = min(lb + stepSize, minmax_ids[1])
+            while lb < minmax_ids[1]:
+                self.stdout.write(f"Collecting views from {lb} to {hb}")
+                views = session.run((
+                    "MATCH (p:Panorama)-[]->(v:View) "
+                    "WHERE "
+                    f"ID(v) >= {lb} "
+                    "AND "
+                    f"ID(v) <= {hb} "
+                    "RETURN ID(p), ID(v), properties(v)"
+                ))
 
-print('Starting session for links')
-with _driver.session() as session:
-    minmax_ids = session.run((
-        "MATCH (a:Panorama)-[l:link]-(b:Panorama) "
-        "RETURN MIN(ID(l)),MAX(ID(l))"
-    ))
-    minmax_ids = minmax_ids.values()[0]
-    print(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
-    stepSize = 100000
-    lb = minmax_ids[0]
-    hb = min(lb + stepSize, minmax_ids[1])
-    while lb < minmax_ids[1]:
-        print(f"Collecting links from {lb} to {hb}")
-        links = session.run((
-            "MATCH (a:Panorama)-[l:link]-(b:Panorama) "
-            "WHERE "
-            f"ID(l) >= {lb} "
-            "AND "
-            f"ID(l) <= {hb} "
-            "RETURN ID(a), ID(b), properties(l)"
-        ))
+                views = views.values()
+                
+                self.stdout.write(f"Writing views from {lb} to {hb} to CSV file")
+                with open('export_views.csv', 'a+') as expfile:
+                    for view in views:
+                        csvview = self.format_view_csv(view)
+                        expfile.write(csvview+'\n')
+                        #break
 
-        links = links.values()
-        
-        print(f"Writing links from {lb} to {hb} to CSV file")
-        with open('export_links.csv', 'a+') as expfile:
-            for link in links:
-                csvlink = format_link_csv(link)
-                expfile.write(csvlink+'\n')
+                lb += stepSize+1
+                hb = min(lb + stepSize, minmax_ids[1])
                 #break
 
-        lb += stepSize+1
-        hb = min(lb + stepSize, minmax_ids[1])
-        #break
+        self.stdout.write('Starting session for filter result')
+        with _driver.session() as session:
+            minmax_ids = session.run((
+                "MATCH (v:View)-[]->(f:FilterResult) "
+                "RETURN MIN(ID(f)),MAX(ID(f))"
+            ))
+            minmax_ids = minmax_ids.values()[0]
+            self.stdout.write(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
+            stepSize = 100000
+            lb = minmax_ids[0]
+            hb = min(lb + stepSize, minmax_ids[1])
+            while lb < minmax_ids[1]:
+                self.stdout.write(f"Collecting filter results from {lb} to {hb}")
+                filter_results = session.run((
+                    "MATCH (v:View)-[]->(f:FilterResult) "
+                    "WHERE "
+                    f"ID(f) >= {lb} "
+                    "AND "
+                    f"ID(f) <= {hb} "
+                    "RETURN ID(v), ID(f), properties(f)"
+                ))
 
-print('Starting session for views')
-with _driver.session() as session:
-    minmax_ids = session.run((
-        "MATCH (p:Panorama)-[]->(v:View) "
-        "RETURN MIN(ID(v)),MAX(ID(v))"
-    ))
-    minmax_ids = minmax_ids.values()[0]
-    print(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
-    stepSize = 100000
-    lb = minmax_ids[0]
-    hb = min(lb + stepSize, minmax_ids[1])
-    while lb < minmax_ids[1]:
-        print(f"Collecting views from {lb} to {hb}")
-        views = session.run((
-            "MATCH (p:Panorama)-[]->(v:View) "
-            "WHERE "
-            f"ID(v) >= {lb} "
-            "AND "
-            f"ID(v) <= {hb} "
-            "RETURN ID(p), ID(v), properties(v)"
-        ))
+                filter_results = filter_results.values()
+                
+                self.stdout.write(f"Writing filter results from {lb} to {hb} to CSV file")
+                with open('export_filter_results.csv', 'a+') as expfile:
+                    for fr in filter_results:
+                        csvfr = self.format_filter_result_csv(fr)
+                        expfile.write(csvfr+'\n')
+                        #break
 
-        views = views.values()
-        
-        print(f"Writing views from {lb} to {hb} to CSV file")
-        with open('export_views.csv', 'a+') as expfile:
-            for view in views:
-                csvview = format_view_csv(view)
-                expfile.write(csvview+'\n')
+                lb += stepSize+1
+                hb = min(lb + stepSize, minmax_ids[1])
                 #break
 
-        lb += stepSize+1
-        hb = min(lb + stepSize, minmax_ids[1])
-        #break
-
-print('Starting session for filter result')
-with _driver.session() as session:
-    minmax_ids = session.run((
-        "MATCH (v:View)-[]->(f:FilterResult) "
-        "RETURN MIN(ID(f)),MAX(ID(f))"
-    ))
-    minmax_ids = minmax_ids.values()[0]
-    print(f"Min ID: {minmax_ids[0]}, Max ID: {minmax_ids[1]}")
-    stepSize = 100000
-    lb = minmax_ids[0]
-    hb = min(lb + stepSize, minmax_ids[1])
-    while lb < minmax_ids[1]:
-        print(f"Collecting filter results from {lb} to {hb}")
-        filter_results = session.run((
-            "MATCH (v:View)-[]->(f:FilterResult) "
-            "WHERE "
-            f"ID(f) >= {lb} "
-            "AND "
-            f"ID(f) <= {hb} "
-            "RETURN ID(v), ID(f), properties(f)"
-        ))
-
-        filter_results = filter_results.values()
-        
-        print(f"Writing filter results from {lb} to {hb} to CSV file")
-        with open('export_filter_results.csv', 'a+') as expfile:
-            for fr in filter_results:
-                csvfr = format_filter_result_csv(fr)
-                expfile.write(csvfr+'\n')
-                #break
-
-        lb += stepSize+1
-        hb = min(lb + stepSize, minmax_ids[1])
-        #break
-
-#format_filter_result_csv
+        #format_filter_result_csv
