cd9360af239718921050e8282b851131a28300b8
Artur Andre <arturpossible@gmail.com>
2019-12-09 18:56:14 -0300

    docker-compose set to auto initialize containers OSMMiner url changed back to inacity.org OSM mirror install_script now use default parameters for neo4j installation

--- django_website/django_website/MapMiners/OSMMiner.py
+++ django_website/django_website/MapMiners/OSMMiner.py
@@ -42,7 +42,9 @@ class OSMMiner(MapMiner):
     # Options are:
     # OverpassAPI.DE -> Public OSM API (with limits of request's size/rate)
     # inacity.org -> INACITY's private mirror of OSM
-    _OSMServerURL = 'OverpassAPI.DE'
+    
+    #_OSMServerURL = 'OverpassAPI.DE'
+    _OSMServerURL = 'inacity.org'
 
     inacityorg = 'inacity.org'
     overpassapi = 'OverpassAPI.DE'
