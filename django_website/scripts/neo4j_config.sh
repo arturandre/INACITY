77474bcd394b8e3876ed1d048fdbb193e0bb9e50
Artur André <arturpossible@gmail.com.com.br>
2020-01-26 20:33:26 -0300

    Fixes and adjustments in neo4j_config script.

--- django_website/scripts/neo4j_config.sh
+++ django_website/scripts/neo4j_config.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 # neo4j_config.sh
 
+
+neo4j_conf=/etc/neo4j/neo4j.conf
+
+while [ ! -f $neo4j_conf ]
+do
+  sleep 2 # or less like 0.2
+  echo "File $neo4j_conf not found yet."
+done
+  echo "File $neo4j_conf found!"
+
+# Stopping the neo4j daemon
+neo4j stop
+
+# Listening on every ip address (instead of only localhost)
+sed -i 's/#dbms.connectors.default_listen_address/dbms.connectors.default_listen_address/g' $neo4j_conf
+
 # Starting the neo4j daemon
 neo4j start
-# Listening on every ip address (instead of only localhost)
-sed -i 's/#dbms.connectors.default_listen_address/dbms.connectors.default_listen_address/g' /etc/neo4j/neo4j.conf
+
 # Setting the default user and pass to 'neo4j' and '1234' (without quote marks)
-curl -H "Content-Type: application/json" -XPOST -d '{"password":"1234"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+while  ! curl -H "Content-Type: application/json" -XPOST -d '{"password":"1234"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password 
+do
+  sleep 2
+done
+
