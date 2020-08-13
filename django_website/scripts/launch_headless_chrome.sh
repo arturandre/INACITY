0916c70f27a49af68534cf765179f36451e2416d
Artur Andre <arturpossible@gmail.com>
2020-08-13 11:42:53 -0300

    improved launcher for headless chrome: - restarting capabilites

--- django_website/scripts/launch_headless_chrome.sh
+++ django_website/scripts/launch_headless_chrome.sh
@@ -1,1 +1,9 @@
-google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost/gsvpanoramacollector/link_browser/default/
+#!/bin/sh
+FILE=./google-chrome_PID
+if test -f "$FILE"; then
+        kill $(cat ./google-chrome_PID)
+        rm -f ./google-chrome_PID
+        echo "$FILE found, restarting headless chrome!"
+fi
+google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost/gsvpanoramacollector/link_browser/default/ &
+echo $! > ./google-chrome_PID
